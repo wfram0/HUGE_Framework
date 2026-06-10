652448462cef2b88e52627bbf6c7d28eedbc04ed
Wolfram26 <105211198+Wolfram26@users.noreply.github.com>
2026-06-10 09:50:11 +0200

    Made the gallery visible and added download functionality

--- application/model/FileModel.php
+++ application/model/FileModel.php
@@ -21,4 +21,63 @@ class FileModel
 
         return $query->fetchAll();
     }
+
+    public static function getById($id)
+    {
+        $db = DatabaseFactory::getFactory()->getConnection();
+
+        $sql = "SELECT * FROM files WHERE id = :id";
+        $query = $db->prepare($sql);
+        $query->execute([':id' => $id]);
+
+        return $query->fetch();
+    }
+
+    public static function deleteFile($id, $userID)
+    {
+        $db = DatabaseFactory::getFactory()->getConnection();
+
+        // Datei holen
+        $sql = "SELECT * FROM files WHERE id = :id AND ownerID = :ownerID";
+        $query = $db->prepare($sql);
+        $query->execute([
+            ':id' => $id,
+            ':ownerID' => $userID
+        ]);
+
+        $file = $query->fetch();
+        if (!$file)
+            return;
+
+        // Pfad
+        $path = dirname(__DIR__, 2)
+            . '/user_pictures/'
+            . $file->ownerID . '/'
+            . $file->name;
+
+        // Datei löschen
+        if (file_exists($path)) {
+            unlink($path);
+        }
+
+        // DB löschen
+        $sql = "DELETE FROM files WHERE id = :id AND ownerID = :ownerID";
+        $query = $db->prepare($sql);
+        $query->execute([
+            ':id' => $id,
+            ':ownerID' => $userID
+        ]);
+    }
+
+    public static function increaseDownload($id)
+    {
+        $db = DatabaseFactory::getFactory()->getConnection();
+
+        $sql = "UPDATE files
+            SET downloads = downloads + 1
+            WHERE id = :id";
+
+        $query = $db->prepare($sql);
+        $query->execute([':id' => $id]);
+    }
 }
