951de878738d0cce0e9e78ba320024d6577e097c
Wolfram26 <105211198+Wolfram26@users.noreply.github.com>
2026-06-22 15:31:05 +0200

    Updated feed page

--- application/view/gallery/feed.php
+++ application/view/gallery/feed.php
@@ -1,17 +1,38 @@
 <div class="container">
     <h1>Globaler Feed</h1>
+
     <?php foreach ($this->files as $file) { ?>
-        <div class=file>
-            <img src="<?= Config::get('URL'); ?>gallery/image/<?= $file->id ?>">
-            <div>
+
+        <?php
+        $isLiked = LikeModel::hasLiked(Session::get('user_id'), $file->id);
+        ?>
+
+        <div class="feed-post">
+
+            <img class="feed-image"
+                 src="<?= Config::get('URL'); ?>gallery/image/<?= $file->id ?>">
+
+            <div class="feed-user">
                 <?= htmlentities($file->user_name); ?>
             </div>
-            <div>
-                <?= $file->likes ?>
+
+            <div class="feed-likes">
+                👍 <?= $file->likes ?>
             </div>
-            <a href="<?= Config::get('URL'); ?>gallery/like/<?= $file->id ?>" ?>
-                Like
-            </a>
+
+            <?php if ($isLiked) { ?>
+                <a class="feed-like"
+                   href="<?= Config::get('URL'); ?>gallery/unlike/<?= $file->id ?>">
+                    Unlike
+                </a>
+            <?php } else { ?>
+                <a class="feed-like"
+                   href="<?= Config::get('URL'); ?>gallery/like/<?= $file->id ?>">
+                    Like
+                </a>
+            <?php } ?>
+
         </div>
+
     <?php } ?>
 </div>
